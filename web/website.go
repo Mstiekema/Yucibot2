fbaec40324b8a7f93c7b3cd9d305983cefbc9c07
Mstiekema <merstiekema@gmail.com>
2017-11-10 22:26:13 +0100

    Added base for songlist page
    Shows history of songlist, copied most HTML / CSS code from previous project because I'm lazy. Still WIP so I'll change the looks in the future. It's working now and that's what it's all about right now.

--- web/website.go
+++ web/website.go
@@ -3,6 +3,8 @@ package web
 import (
   "strings"
   "html/template"
+  "time"
+  "strconv"
   "fmt"
   "net/http"
   "log"
@@ -21,6 +23,7 @@ func MainWeb() {
   http.HandleFunc("/", home)
   http.HandleFunc("/test", test)
   http.HandleFunc("/user/", userPage)
+  http.HandleFunc("/songlist/", songlist)
   
   err := http.ListenAndServe(":9090", nil)
   if err != nil {
@@ -55,6 +58,84 @@ func userPage(w http.ResponseWriter, r *http.Request) {
   if err != nil {
     log.Print("template executing error: ", err)
   }
+}
+
+func songlist(w http.ResponseWriter, r *http.Request) {
+  date := strings.Replace(strings.SplitAfter(r.URL.Path, "/")[2], "/", "", 2)
+  if date == "" {
+    y, m, d := time.Now().Date()
+    date = strconv.Itoa(y)+"-"+strconv.Itoa(int(m))+"-"+strconv.Itoa(d)
+  }
+  
+  var db = base.Conn()
+  res, err := db.Query("SELECT name, title, thumb, length, songid, playState FROM songrequest WHERE DATE(time) = '"+date+"'")
+  if err != nil {
+    panic(err.Error())
+  }
+  defer res.Close()
+  
+  //Songlist
+  var names []string
+  var titles []string
+  var thumbs []string
+  var lengths []string
+  var songids []string
+  var playStates []string
+  var currSongT string
+  var currSongId string
+  var currSongN string
+  
+  for res.Next() {
+    var name string
+    var title string
+    var thumb string
+    var length string
+    var songid string
+    var playState string
+    err = res.Scan(&name, &title, &thumb, &length, &songid, &playState)
+    names = append(names, name)
+    titles = append(titles, title)
+    thumbs = append(thumbs, thumb)
+    lengths = append(lengths, length)
+    songids = append(songids, songid)
+    playStates = append(playStates, playState)
+  }
+  
+  for i, v := range playStates {
+    if v == "0" {
+      currSongT = titles[i]
+      currSongId = songids[i]
+      currSongN = names[i]
+      break
+    } else {
+      currSongT = ""
+      currSongId = ""
+      currSongN = ""
+    }
+  }
+  
+  Songs := map[string]interface{}{
+    "Date": date,
+    "CurrSongN": currSongN,
+    "CurrSongT": currSongT,
+    "CurrSongId": currSongId,
+    "Name": names,
+    "Title": titles,
+    "Thumb": thumbs,
+    "Length": lengths,
+    "Songid": songids,
+    "PlayState": playStates,
+  }
+  
+  t, err := template.ParseFiles("./web/templates/songlist.html")
+  if err != nil {
+    log.Print("template parsing error: ", err)
+  }
+  err = t.Execute(w, Songs)
+  if err != nil {
+    log.Print("template executing error: ", err)
+  }
+  db.Close()
 }
 
 func test(w http.ResponseWriter, r *http.Request) {
