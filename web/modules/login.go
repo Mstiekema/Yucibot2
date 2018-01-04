32ac5fc51b8d2ec6ded9e5ecd4bab0e257f45b98
Mstiekema <merstiekema@gmail.com>
2018-01-05 00:15:55 +0100

    Improved menu
    My CSS is still shit.

--- web/modules/login.go
+++ web/modules/login.go
@@ -19,6 +19,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
   session.Values["userId"] = user.UserID
   session.Values["username"] = user.Name
   session.Values["displayName"] = user.NickName
+  session.Values["profile_pic"] = user.AvatarURL
+  session.Values["points"] = base.Query("SELECT points FROM user WHERE name = '"+user.Name+"'")
+  session.Values["lines"] = base.Query("SELECT num_lines FROM user WHERE name = '"+user.Name+"'")
   session.Values["level"] = base.Query("SELECT level FROM user WHERE name = '"+user.Name+"'")
   session.Save(r, w)
   
@@ -33,6 +36,9 @@ func Logout(w http.ResponseWriter, r *http.Request) {
   session.Values["userId"] = nil
   session.Values["username"] = nil
   session.Values["displayName"] = nil
+  session.Values["profile_pic"] = nil  
+  session.Values["points"] = nil
+  session.Values["lines"] = nil
   session.Values["level"] = nil
   session.Save(r, w)
   
--- web/static/basic.js
+++ web/static/basic.js
@@ -5,4 +5,20 @@ $(document).ready(function(){
       location.href = "/user/" + name;
     }
   });
+  
+  $("#fullProfile").click(function() {
+    $(".profile").toggleClass("hidden")
+    $("#name").toggleClass("hover")
+  });
+  
+  document.onclick = function(event) {
+    if (event.target.id == "name" || $(event.target).attr('class') == "profile text-left" || event.target.id == "menu" || $(event.target).attr('class') == "fa fa-caret-down") {
+      return
+    } else if( $(event.target).attr('class') == "img-circle pf") {
+      window.location.href = "/user/" + event.target.id
+    } else {
+      $('.profile').addClass('hidden');
+      $("#name").removeClass("hover")
+    }
+  }  
 });
